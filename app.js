376198048a1da6da16e85423e815db20c2a2ffe9
Om Prakash Sharma <omsharma1510@gmail.com>
2023-02-27 20:23:58 +0530

    - update search api

--- app.js
+++ app.js
@@ -14,8 +14,8 @@ Neo4J.getInstance();
 
 app.post('/api/search', async (req, res) => {
     try {
-        const keyword = req.body.search;
-        const result = await Neo4J.getInstance().executeQuery(keyword);
+        const query = generateQueryToEexecute(req.body);
+        const result = await Neo4J.getInstance().executeQuery(query);
         const headerList = getHeaders(result || [])
         if (result.length) {
             result.forEach(row => {
@@ -36,6 +36,23 @@ app.post('/api/search', async (req, res) => {
         return res.status(500).send({ msg: error.message })
     }
 })
+
+function generateQueryToEexecute(payload = {}) {
+    const { search = null, model = 'Customer', filter = {}, limit = 10 } = payload;
+
+    // if we have query in body then return
+    if (search && typeof search === 'string') {
+        return search;
+    }
+
+    const filterQuery = Object.keys(filter).reduce((result, ele) => {
+        result = (result != '' ? `${result} AND ` : '') + (` e.${ele} = '${filter[ele]}'`);
+        return result;
+    }, '');
+
+    const queryStr = `MATCH (e:${model}) ${filterQuery != '' ? ` WHERE ${filterQuery}` : ''} RETURN e LIMIT ${limit}`;
+    return queryStr;
+}
 
 function getHeaders(list = []) {
     return [...list.reduce((res, e) => {
